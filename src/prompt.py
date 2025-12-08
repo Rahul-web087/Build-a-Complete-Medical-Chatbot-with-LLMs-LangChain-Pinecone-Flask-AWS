996340cadd3d88d729db71e7ed24616b1b3f1ddc
Rahul Nayak <132944744+Rahul-web087@users.noreply.github.com>
2025-12-09 03:36:56 +0530

    Modify system prompt for medical assistant
    Updated the system prompt for the medical assistant.

--- src/prompt.py
+++ src/prompt.py
@@ -1,13 +1,3 @@
-# from langchain_core.prompts import ChatPromptTemplate
-#
-# system_prompt = "Answer the user's questions based on the below context:\n\n{context}"
-# user_prompt = "{input}"
-#
-# prompt = ChatPromptTemplate.from_messages([
-#     ("system", system_prompt),
-#     ("user", user_prompt),
-# ])
-
 system_prompt = (
     "You are an medical assistant for question answering tasks. "
     "Use the following pieces of retrieved context to answer "
